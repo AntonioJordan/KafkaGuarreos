fa27704c1aa10f5af6e0662af8050fffd0a57fcd
AntonioJordan <tonijordanrodriguez6@gmail.com>
2024-07-12 01:12:15 +0200

    Poh ahi que amoh paco

---
+++ src/test/java/com/kafkaguarreo/kafkaguarreo/KafkaguarreoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkaguarreo.kafkaguarreo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaguarreoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
